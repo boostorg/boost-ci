ad8f9b438d146ccd090ee154144e9a1c2b8f7b22
James E. King III <jking@apache.org>
2018-09-03 02:31:54 +0000

    fix icu build

--- ci/appveyor/install_icu.bat
+++ ci/appveyor/install_icu.bat
@@ -4,9 +4,9 @@
 :: Boost Software License, Version 1.0. (See accompanying file
 :: LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-:: Installs icu using vcpkg
-:: Requires: "BITS" set to either "x86" or "x64"
+:: Installs ICU using vcpkg
+:: Requires: "ICU_BITS" set to either "x86" or "x64"
 
 @ECHO ON
-C:\tools\vcpkg\vcpkg.exe install icu:%BITS%-windows || EXIT /B
-SET ICU_PATH=C:\tools\vcpkg\packages\icu_%BITS%-windows
+C:\tools\vcpkg\vcpkg.exe install icu:%ICU_BITS%-windows || EXIT /B
+SET ICU_PATH=C:\tools\vcpkg\packages\icu_%ICU_BITS%-windows
