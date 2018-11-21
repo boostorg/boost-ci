b65094f82649ae0d67cd9e1433c1f9b6a1b1a839
James E. King III <jking@apache.org>
2018-11-21 08:30:49 -0500

    install required dependencies for valgrind

--- ci/travis/valgrind.sh
+++ ci/travis/valgrind.sh
@@ -16,6 +16,9 @@ if [[ -z "$CXXSTD" ]]; then
     CXXSTD=03
 fi
 
+# required for valgrind to work:
+apt-get install libc6-dbg
+
 # valgrind on travis is 3.10 which is old
 # using valgrind 3.14 but we have to build it
 
