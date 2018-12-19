bc32fe48eea9f5230930ba73a111d3654f42f91c
James E. King III <jking@apache.org>
2018-12-19 14:58:47 -0500

    update install prerequisites

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -2,9 +2,10 @@
 cd .. || EXIT /B
 git clone -b %APPVEYOR_REPO_BRANCH% --depth 1 https://github.com/boostorg/boost.git boost-root || EXIT /B
 cd boost-root || EXIT /B
+git submodule update -q --init libs/headers || EXIT /B
+git submodule update -q --init tools/boost_install || EXIT /B
 git submodule update -q --init tools/boostdep || EXIT /B
 git submodule update -q --init tools/build || EXIT /B
-git submodule update -q --init tools/inspect || EXIT /B
 xcopy /s /e /q %APPVEYOR_BUILD_FOLDER% libs\%SELF% || EXIT /B
 python tools/boostdep/depinst/depinst.py --include benchmark --include example --include examples --include tools %DEPINST% %SELF:\=/% || EXIT /B
 cmd /c bootstrap || EXIT /B
--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -22,13 +22,13 @@ if [ "$SELF" == "interval" ]; then
 fi
 git clone -b $TRAVIS_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
 cd boost-root
+git submodule update -q --init libs/headers
+git submodule update -q --init tools/boost_install
 git submodule update -q --init tools/boostdep
 git submodule update -q --init tools/build
-git submodule update -q --init tools/inspect
 cp -r $TRAVIS_BUILD_DIR/* libs/$SELF
 export BOOST_ROOT="`pwd`"
 export PATH="`pwd`":$PATH
 python tools/boostdep/depinst/depinst.py --include benchmark --include example --include examples --include tools $DEPINST $SELF
 ./bootstrap.sh
 ./b2 headers
-
