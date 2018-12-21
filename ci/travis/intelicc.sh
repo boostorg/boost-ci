f732c2f796d3d2c77bbb5f44401516e25ee03841
James E. King III <jking@apache.org>
2018-12-21 16:31:39 -0500

    security fix for intelicc

--- ci/travis/intelicc.sh
+++ ci/travis/intelicc.sh
@@ -17,10 +17,16 @@
 
 set -ex
 
+function finish {
+  rm -rf /tmp/parallel_studio_xe_2019_update1_professional_edition_for_cpp_online/silent.cfg
+}
+
 pushd /tmp
+sudo apt-get install cpio
 wget http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/14857/parallel_studio_xe_2019_update1_professional_edition_for_cpp_online.tgz
 tar xzf parallel_studio_xe_2019_update1_professional_edition_for_cpp_online.tgz
 cd parallel_studio_xe_2019_update1_professional_edition_for_cpp_online/
+trap finish EXIT
 cat << EOF > silent.cfg
 ACCEPT_EULA=accept
 CONTINUE_WITH_OPTIONAL_ERROR=yes
@@ -41,8 +47,8 @@ INTEL_SW_IMPROVEMENT_PROGRAM_CONSENT=no
 ARCH_SELECTED=ALL
 COMPONENTS=;intel-conda-index-tool__x86_64;intel-comp__x86_64;intel-comp-32bit__x86_64;intel-comp-doc__noarch;intel-comp-l-all-common__noarch;intel-comp-l-all-vars__noarch;intel-comp-nomcu-vars__noarch;intel-comp-ps-32bit__x86_64;intel-comp-ps__x86_64;intel-comp-ps-ss__x86_64;intel-comp-ps-ss-bec__x86_64;intel-comp-ps-ss-bec-32bit__x86_64;intel-openmp__x86_64;intel-openmp-32bit__x86_64;intel-openmp-common__noarch;intel-openmp-common-icc__noarch;intel-tbb-libs-32bit__x86_64;intel-tbb-libs__x86_64;intel-idesupport-icc-common-ps__noarch;intel-conda-icc_rt-linux-64-shadow-package__x86_64;intel-icc__x86_64;intel-icc-32bit__x86_64;intel-c-comp-common__noarch;intel-icc-common__noarch;intel-icc-common-ps__noarch;intel-icc-common-ps-ss-bec__noarch;intel-icc-doc__noarch;intel-icc-doc-ps__noarch;intel-icc-ps__x86_64;intel-icc-ps-ss-bec__x86_64;intel-icc-ps-ss-bec-32bit__x86_64;intel-tbb-devel-32bit__x86_64;intel-tbb-devel__x86_64;intel-tbb-common__noarch;intel-tbb-doc__noarch;intel-conda-tbb-linux-64-shadow-package__x86_64;intel-conda-tbb-linux-32-shadow-package__x86_64;intel-conda-tbb-devel-linux-64-shadow-package__x86_64;intel-conda-tbb-devel-linux-32-shadow-package__x86_64;intel-ism__noarch;intel-ipsc__noarch;intel-psxe-common__noarch;intel-psxe-doc__noarch;intel-psxe-common-doc__noarch;intel-ips-doc__noarch;intel-psxe-licensing__noarch;intel-psxe-licensing-doc__noarch;intel-icsxe-pset
 EOF
-sudo apt-get install cpio
 sudo ./install.sh -s silent.cfg
+rm -f silent.cfg
 export PATH=/opt/intel/bin:$PATH
 popd
 cd ../..
