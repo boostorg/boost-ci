08957ca0845d052bd008a44fac17ac48d687482b
James E. King III <jking@apache.org>
2019-04-18 19:55:29 -0400

    Add diagnostic output when bootstrap fails on appveyor

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -8,5 +8,9 @@ git submodule update -q --init tools/boostdep || EXIT /B
 git submodule update -q --init tools/build || EXIT /B
 xcopy /s /e /q /I %APPVEYOR_BUILD_FOLDER% libs\%SELF% || EXIT /B
 python tools/boostdep/depinst/depinst.py --include benchmark --include example --include examples --include tools %DEPINST% %SELF:\=/% || EXIT /B
-cmd /c bootstrap || EXIT /B
+cmd /c bootstrap
+IF NOT %ERRORLEVEL% == 0 (
+    type bootstrap.log
+    EXIT /B 1
+)
 b2 headers
