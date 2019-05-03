4301f6471e95a2a6499b90666e83dd4d20c17774
Mateusz Łoskot <mateusz@loskot.net>
2019-05-04 00:53:57 +0200

    Add configuration for Azure Pipelines (AzP)
    First stab at the common AzP configuration following the convention
    structure, build jobs and build steps as established by the existing
    configuration for Travis CI and AppVeyor.

---
+++ ci/azure-pipelines/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# Copyright 2017 - 2019 James E. King III
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#      http://www.boost.org/LICENSE_1_0.txt)
+#
+# Bash script to run in Linux images of Azure Pipelines to
+# perform a bjam build.
+# The cwd should be $BOOST_ROOT/libs/$SELF before running.
+#
+set -ex
+
+. $(dirname "${BASH_SOURCE[0]}")/enforce.sh
+
+$BOOST_ROOT/b2 . toolset=$B2_TOOLSET cxxstd=$B2_CXXSTD $B2_CXXFLAGS $B2_DEFINES $B2_INCLUDE $B2_LINKFLAGS $B2_TESTFLAGS $B2_ADDRESS_MODEL $B2_LINK $B2_THREADING $B2_VARIANT -j${B2_JOBS} $*
