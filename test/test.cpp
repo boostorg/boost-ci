45c1963fa147f2feb3415d61aff593fae9eb3af2
Mateusz Łoskot <mateusz@loskot.net>
2020-02-29 23:35:18 +0100

    Add test/ with basic health checks to be run on supported CI-s
    Add fake library header for depinst.py.

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2020 Mateusz Loskot <mateusz@loskot.net>
+//
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/core/lightweight_test.hpp>
+
+int main()
+{
+    return boost::report_errors();
+}
