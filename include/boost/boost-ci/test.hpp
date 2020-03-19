6c354befa87ac2221cd1faebf7443b04bfbebc81
Mateusz Łoskot <mateusz@loskot.net>
2020-03-19 21:56:00 +0100

    Set up boost-ci self-test to be run on supported CI-s (#37)
    Enable AppVeyor, Azure Pipelines and Travis CI for self.
    Add test/ with basic health checks to be run on supported CI-s.
    Add fake library header for depinst.py.
    Disable codecov.io for boost-ci self-check on Travis CI.
    
    Requires change in #35

---
+++ include/boost/boost-ci/test.hpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2020 Mateusz Loskot <mateusz@loskot.net>
+//
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// This header is for depinst.py to ensure it deploys dependencies
+// required by test/test.cpp
+#include <boost/core/lightweight_test.hpp>
