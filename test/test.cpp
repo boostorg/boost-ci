57f848ec012a80f9855e494c9de41f12cd21874e
Alexander Grund <git@grundis.de>
2020-04-07 11:13:24 +0200

    Fix failure of codecov job

--- test/test.cpp
+++ test/test.cpp
@@ -5,9 +5,11 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <boost/boost-ci/boost_ci.hpp>
 #include <boost/core/lightweight_test.hpp>
 
 int main()
 {
+    BOOST_TEST_EQ(boost::boost_ci::get_answer(), 42);
     return boost::report_errors();
 }
