bd1e958e739c96b17df74ef2b73a1abdcf5529d0
Alexander Grund <Flamefire@users.noreply.github.com>
2021-11-15 20:43:00 +0100

    Split coverage into setup and upload
    Build is done manually by the regular build.sh script. All we need is the setup (setting of CXXFLAGS etc.) and the upload part.
    This allows to easily run multiple build runs (e.g. in different configurations)
    
    Closes #94

---
+++ ci/github/codecov.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# Copyright 2021 Alexander Grund
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#      http://www.boost.org/LICENSE_1_0.txt)
+#
+# Bash script to run on Github to perform codecov.io integration
+#
+
+set -ex
+
+source $(dirname "${BASH_SOURCE[0]}")/../codecov.sh "$1"
+
+if [[ "$1" == "setup" ]]; then
+    echo "B2_VARIANT=$B2_VARIANT" >> "$GITHUB_ENV"
+    echo "B2_CXXFLAGS=$B2_CXXFLAGS" >> "$GITHUB_ENV"
+    echo "B2_LINKFLAGS=$B2_LINKFLAGS" >> "$GITHUB_ENV"
+fi
--- ci/travis/codecov.sh
+++ ci/travis/codecov.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 # Copyright 2017 - 2019 James E. King III
+# Copyright 2021 Alexander Grund
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 #      http://www.boost.org/LICENSE_1_0.txt)
@@ -10,5 +11,8 @@
 
 set -ex
 
-$(dirname "${BASH_SOURCE[0]}")/../codecov.sh "collect"
-$(dirname "${BASH_SOURCE[0]}")/../codecov.sh "upload"
+CI_DIR="$(dirname "${BASH_SOURCE[0]}")/.."
+
+source "$CI_DIR"/codecov.sh "setup"
+"$CI_DIR"/build.sh
+"$CI_DIR"/codecov.sh "upload"
