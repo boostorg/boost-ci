214c7a06becf65e18177a661a39f22270fe51e95
Alexander Grund <Flamefire@users.noreply.github.com>
2021-11-19 00:38:58 +0100

    Enhance package installation on AzP and move more tasks into common_install.sh
    - Deduce PACKAGES from CXX/B2_COMPILER
    - Deduce B2_TOOLSET from B2_COMPILER
    - Write the correct user-config.jam in common_install.sh
    - Print compiler location and version in common_install.sh

--- ci/github/install.sh
+++ ci/github/install.sh
@@ -20,37 +20,6 @@ export BOOST_CI_SRC_FOLDER="${GITHUB_WORKSPACE//\\//}"
 echo "BOOST_CI_TARGET_BRANCH=$BOOST_CI_TARGET_BRANCH" >> $GITHUB_ENV
 echo "BOOST_CI_SRC_FOLDER=$BOOST_CI_SRC_FOLDER" >> $GITHUB_ENV
 
-# B2_COMPILER is optional, e.g. unset for CMake builds -> Only checkout
-if [ -n "$B2_COMPILER" ]; then
-    echo "B2_COMPILER=$B2_COMPILER" >> $GITHUB_ENV
-    if [[ "$B2_COMPILER" == "clang" ]] || [[ "$B2_COMPILER" == clang-* ]]; then
-      B2_TOOLSET=clang
-      export CXX=${B2_COMPILER/clang/clang++}
-    elif [[ "$B2_COMPILER" =~ gcc ]]; then
-      B2_TOOLSET=gcc
-      export CXX=${B2_COMPILER/gcc/g++}
-    else
-      echo "Unknown compiler: '$B2_COMPILER'. Need either clang(-version) or gcc(-version)" >&2
-      false
-    fi
-    if [[ "$B2_COMPILER" == clang-* ]]; then
-      llvmPath="/usr/lib/llvm-${B2_COMPILER#*-}/bin"
-      echo "$llvmPath" >> $GITHUB_PATH
-    fi
-
-    echo "Compiler location: $(command -v $CXX)"
-    echo -n "using $B2_TOOLSET : : $CXX" > ~/user-config.jam
-    if [ -n "$GCC_TOOLCHAIN_ROOT" ]; then
-        echo -n " : <compileflags>\"--gcc-toolchain=$GCC_TOOLCHAIN_ROOT\" <linkflags>\"--gcc-toolchain=$GCC_TOOLCHAIN_ROOT\"" >> ~/user-config.jam
-    fi
-    echo " ;" >> ~/user-config.jam
-
-    $B2_COMPILER --version
-    $CXX --version
-
-    echo "B2_TOOLSET=$B2_TOOLSET" >> $GITHUB_ENV
-fi
-
 echo "B2_CXXSTD=$B2_CXXSTD" >> $GITHUB_ENV
 if [[ "$B2_SANITIZE" == "yes" ]]; then
   echo "B2_ASAN=1" >> $GITHUB_ENV
@@ -63,3 +32,5 @@ fi
 . $(dirname "${BASH_SOURCE[0]}")/../common_install.sh
 echo "SELF=$SELF" >> $GITHUB_ENV
 echo "BOOST_ROOT=$BOOST_ROOT" >> $GITHUB_ENV
+echo "B2_TOOLSET=$B2_TOOLSET" >> $GITHUB_ENV
+echo "B2_COMPILER=$B2_COMPILER" >> $GITHUB_ENV
