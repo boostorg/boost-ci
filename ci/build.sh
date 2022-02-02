2b79a823fbf8df05ba68a652d49c957bd9668d7d
James E. King III <jking@apache.org>
2022-02-02 06:24:58 -0500

    gha multiarch bigendian build support

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright 2017 - 2019 James E. King III
+# Copyright 2017 - 2022 James E. King III
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 #      http://www.boost.org/LICENSE_1_0.txt)
@@ -18,7 +18,7 @@ export UBSAN_OPTIONS=print_stacktrace=1
 
 cd "$BOOST_ROOT"
 
-./b2 ${B2_TARGETS} "${B2_ARGS[@]}" "$@"
+${B2_WRAPPER} ./b2 ${B2_TARGETS} "${B2_ARGS[@]}" "$@"
 
 if [ "$B2_USE_CCACHE" == "1" ] && command -v ccache &> /dev/null; then
   echo "CCache summary"
