f39ff8c4b19ac03ca5897013fd4d086661ca842b
Alexander Grund <Flamefire@users.noreply.github.com>
2022-06-08 15:00:51 +0200

    Add flag to use `testing.launcher` for Windows coverage collection
    OpenCppCoverage traces executed binaries to collect coverage data. This by now includes compilers which slows down the build.
    Use the `b2` `testing.launcher` feature to only run the tests with that tracing.
    As that feature may already be used by tests (e.g. in the Jamfiles) make it optional.

--- ci/opencppcoverage.ps1
+++ ci/opencppcoverage.ps1
@@ -15,11 +15,11 @@ $env:B2_VARIANT = "debug"
 mkdir __out
 
 # Build command to collect coverage
-$cmd = 'OpenCppCoverage.exe --export_type cobertura:__out/cobertura.xml --modules "{0}" ' -f ${env:BOOST_ROOT}
+$cmd = "OpenCppCoverage.exe --export_type cobertura:__out/cobertura.xml --modules '{0}' " -f ${env:BOOST_ROOT}
 # Include own headers (relocated to boost\*)
-$cmd += (Get-ChildItem -Name "${env:BOOST_CI_SRC_FOLDER}\include\boost").ForEach({'--sources "boost\{0}"' -f $_}) -join " "
+$cmd += (Get-ChildItem -Name "${env:BOOST_CI_SRC_FOLDER}\include\boost").ForEach({"--sources 'boost\{0}'" -f $_}) -join " "
 # Include own cpp files
-$cmd += " --sources ${env:BOOST_ROOT}\libs\${env:SELF} "
+$cmd += " --sources '${env:BOOST_ROOT}\libs\${env:SELF}' "
 $exclusions = @(
   # Lines marked with LCov or coverity exclusion comments
   '.*// LCOV_EXCL_LINE'
@@ -31,8 +31,17 @@ $exclusions = @(
 )
 $cmd += $exclusions.ForEach({"--excluded_line_regex '{0}'" -f $_}) -join " "
 # Cover all subprocess of the build script (-> b2 -> test binary)
-$cmd += "--cover_children -- cmd.exe /c $scriptPath\build.bat"
+$cmd += " --cover_children -- "
 
-# Print generated command and run
-$cmd
-Invoke-Expression $cmd
+If ($env:B2_ALLOW_COVERAGE_TEST_LAUNCHER -eq "1") {
+    $env:B2_FLAGS += 'testing.launcher="' + $cmd + '"'
+    echo "Using coverage launcher: $cmd"
+    echo "Starting build with coverage collection"
+    Invoke-Expression "cmd.exe /c $scriptPath\build.bat"
+} Else {
+    $cmd += "cmd.exe /c $scriptPath\build.bat"
+    # Print generated command and run
+    $cmd
+    echo "Starting build with coverage collection"
+    Invoke-Expression $cmd
+}
