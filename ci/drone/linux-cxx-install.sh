30e8d40f0c6cc91f3e81c4d4316b48d9f7a4a97f
Alexander Grund <Flamefire@users.noreply.github.com>
2022-10-16 13:23:25 +0200

    Fix & Refactor linux-cxx-install.sh
    - Wrap retry of adding a repository in a function to avoid flakiness
    - Update APT after adding $SOURCES
    - Use `[ ]` instead of `test`
    - Reduce verbosity (Replace `set -x` by `echo`)

--- ci/drone/linux-cxx-install.sh
+++ ci/drone/linux-cxx-install.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-set -ex
-echo ">>>>> APT: REPO.."
+set -e
+
+function add_repository {
+    name="$1"
+    echo -e "\tAdding repository $name"
+    for i in {1..3}; do sudo -E apt-add-repository -y "$name" && return 0 || sleep 10; done
+    return 1 # Failed
+}
+
+echo ">>>>> APT: REPOSITORIES..."
+
 if [ "$UBUNTU_TOOLCHAIN_DISABLE" != "true" ]; then
-    for i in {1..3}; do sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test" && break || sleep 10; done
+    add_repository "ppa:ubuntu-toolchain-r/test"
 else
     echo "UBUNTU_TOOLCHAIN_DISABLE is 'true'. Not installing ppa:ubuntu-toolchain-r/test"
 fi
 
-if test -n "${LLVM_OS}" ; then
+if [ -n "${LLVM_OS}" ]; then
+    echo ">>>>> APT: INSTALL LLVM repo"
     curl -sSL --retry 5 https://apt.llvm.org/llvm-snapshot.gpg.key | sudo gpg --dearmor > /etc/apt/trusted.gpg.d/llvm-snapshot.gpg
-    if test -n "${LLVM_VER}" ; then
-        sudo -E apt-add-repository "deb http://apt.llvm.org/${LLVM_OS}/ llvm-toolchain-${LLVM_OS}-${LLVM_VER} main"
+    if [ -n "${LLVM_VER}" ]; then
+        llvm_toolchain="llvm-toolchain-${LLVM_OS}-${LLVM_VER}"
     else
-        # Snapshot (i.e. trunk) build of clang
-        sudo -E apt-add-repository "deb http://apt.llvm.org/${LLVM_OS}/ llvm-toolchain-${LLVM_OS} main"
+        # Snapshot (i.e. trunk) build
+        llvm_toolchain="llvm-toolchain-${LLVM_OS}"
     fi
+    add_repository "deb http://apt.llvm.org/${LLVM_OS}/ ${llvm_toolchain} main"
 fi
-echo ">>>>> APT: UPDATE.."
-sudo -E apt-get -o Acquire::Retries=3 update
-if test -n "${SOURCES}" ; then
-    echo ">>>>> APT: INSTALL SOURCES.."
+
+if [ -n "${SOURCES}" ]; then
+    echo ">>>>> APT: INSTALL PPAs from \$SOURCES..."
     for SOURCE in $SOURCES; do
-        sudo -E apt-add-repository ppa:$SOURCE
+        add_repository "ppa:$SOURCE"
     done
 fi
-echo ">>>>> APT: INSTALL ${PACKAGES}.."
+
+echo ">>>>> APT: UPDATE..."
+sudo -E apt-get -o Acquire::Retries=3 update
+
+echo ">>>>> APT: INSTALL ${PACKAGES}..."
 sudo -E DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::Retries=3 -y --no-install-suggests --no-install-recommends install ${PACKAGES}
