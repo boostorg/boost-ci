e1c08bd2ea97c9ab3bc2ee052bcf230631889c6d
sdarwin <samuel.d.darwin@gmail.com>
2023-02-09 15:00:43 -0700

    Add variable BOOST_CI_CODECOV_IO_UPLOAD to allow skipping section

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -83,24 +83,27 @@ elif [[ "$1" == "upload" ]]; then
     # pull request)
     lcov --rc lcov_branch_coverage=${LCOV_BRANCH_COVERAGE} --list coverage.info
 
-    #
-    # upload to codecov.io
-    #
-    curl -Os https://uploader.codecov.io/latest/linux/codecov
-    
-    # Verify Download
-    if command -v gpg &> /dev/null && command -v gpgv &> /dev/null; then
-        curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+    if [[ "$BOOST_CI_CODECOV_IO_UPLOAD" != "skip" ]]; then
+        #
+        # upload to codecov.io
+        #
+        curl -Os https://uploader.codecov.io/latest/linux/codecov
 
-        curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-        curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+        # Verify Download
+        if command -v gpg &> /dev/null && command -v gpgv &> /dev/null; then
+            curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
 
-        gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
-        shasum -a 256 -c codecov.SHA256SUM
+            curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+            curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+
+            gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+            shasum -a 256 -c codecov.SHA256SUM
+        fi
+
+        chmod +x codecov
+        ./codecov --verbose --nonZero ${CODECOV_NAME:+--name "$CODECOV_NAME"} -f coverage.info -X search
+        # end of [[ "$BOOST_CI_CODECOV_IO_UPLOAD" != "skip" ]] section
     fi
-
-    chmod +x codecov
-    ./codecov --verbose --nonZero ${CODECOV_NAME:+--name "$CODECOV_NAME"} -f coverage.info -X search
 else
     echo "Invalid parameter for codecov.sh: '$1'." >&2
     false
