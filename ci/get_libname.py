24d98336a1c6e63f65ecca03bd5d04da4173b3a9
Alexander Grund <Flamefire@users.noreply.github.com>
2023-12-23 11:14:13 +0100

    get_libname: Add license

--- ci/get_libname.py
+++ ci/get_libname.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python 
 
+# Copyright Alexander Grund 2021-2023
+#
+# Distributed under the Boost Software License, Version 1.0.
+# https://www.boost.org/LICENSE_1_0.txt
+
 import json
 import os
 import sys
