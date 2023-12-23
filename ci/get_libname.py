f1dd8c8c4e70c5a995132c9a37d5d0514e690db9
Alexander Grund <Flamefire@users.noreply.github.com>
2023-12-23 10:56:51 +0100

    get_libname: Support a list of libraries in libraries.json
    Currently the script expects `libraries.json` to contain a dict with the
    library metadata.
    However it can contain information about multiple sub-libraries,
    e.g. in Boost.Utility or Boost.Functional in which case the top-level
    struct is a list containing a dict for each sub-library.
    It is also possible to wrap the single dict in a list with a single item
    which should be equivalent to having the dict at the top-level.
    
    Modify the script to detect such a list and work on the first entry.

--- ci/get_libname.py
+++ ci/get_libname.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python 
 
-import os
 import json
+import os
+import sys
 
 with open(os.path.join(os.environ['BOOST_CI_SRC_FOLDER'], 'meta', 'libraries.json')) as jsonFile:
     lib_data = json.load(jsonFile)
+    if isinstance(lib_data, (list, tuple)):
+      if len(lib_data) > 1:
+        sys.stderr.write('Found multiple libraries in meta/libraries.json. Assuming first entry is the main one.\n')
+      else:
+        sys.stderr.write('Unwrapping list with single entry in meta/libraries.json.\n')
+      lib_data = lib_data[0]
     print(lib_data['key'])
