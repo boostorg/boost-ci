d37f30db918a95b362d41827a735d01b48d16e9a
Alexander Grund <Flamefire@users.noreply.github.com>
2025-10-14 20:30:30 +0200

    Verify existance of brew installed clang

--- ci/github/install.sh
+++ ci/github/install.sh
@@ -38,18 +38,30 @@ if [[ "$B2_SANITIZE" == "yes" ]]; then
 fi
 
 if [[ "$RUNNER_OS" == "macOS" ]] && [[ "${B2_COMPILER:-}" =~ "clang-" ]] && ! command -v "$B2_COMPILER"; then
+    system_clang_version=$(clang --version)
     { set +x; } &> /dev/null
     clang_version=${B2_COMPILER#clang-}
-    if [[ $(clang --version) == *"clang version ${clang_version}."* ]]; then
+    if [[ ${system_clang_version} == *"clang version ${clang_version}."* ]]; then
+        echo "Using system clang: $(command -v clang)"
         B2_COMPILER=clang
     else
         # When the default clang doesn't match the requested version try using the brew installed one
         if brew_clang_prefix=$(brew --prefix "llvm@$clang_version"); then
-          echo "$brew_clang_prefix/bin" >> "$GITHUB_PATH"
-          echo "Found Clangs in HomeBrew: " "$brew_clang_prefix/bin/"clang* /opt/homebrew/opt/llvm/bin/clang*
-          export PATH="$brew_clang_prefix/bin:$PATH"
-          echo "Clang to be used: $(command -v clang)"
-          B2_COMPILER=clang
+          if [[ -f "$brew_clang_prefix/bin/clang" ]]; then
+              echo "$brew_clang_prefix/bin" >> "$GITHUB_PATH"
+              echo "Found Clangs in HomeBrew: " "$brew_clang_prefix/bin/"clang* /opt/homebrew/opt/llvm/bin/clang*
+              export PATH="$brew_clang_prefix/bin:$PATH"
+              echo "Clang to be used: $(command -v clang)"
+              B2_COMPILER=clang
+          else
+            echo "Failed to find Clang $clang_version as requested from B2_COMPILER=${B2_COMPILER} in system or $brew_clang_prefix"
+            if [[ -d $brew_clang_prefix ]]; then
+              echo "Available brew binaries: $(ls "$brew_clang_prefix/bin")"
+            else
+              echo "HomeBrew installation is missing"
+            fi
+            exit 1
+          fi
         else
             echo "Failed to find Clang $clang_version as requested from B2_COMPILER=${B2_COMPILER}"
             exit 1
