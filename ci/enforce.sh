a6168c62981afc3cd01a6014dd8cbaf385192a82
Alexander Grund <Flamefire@users.noreply.github.com>
2026-05-09 20:22:12 +0200

    Compose B2 arguments for Windows in `enforce.sh`
    Windows CMD is not powerful enough for the correct translation of quoted
    B2 input variables like `B2_DEFINE`

--- ci/enforce.sh
+++ ci/enforce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright 2017 - 2019 James E. King III
-# Copyright 2020 - 2025 Alexander Grund
+# Copyright 2020 - 2026 Alexander Grund
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 #      http://www.boost.org/LICENSE_1_0.txt)
@@ -60,8 +60,13 @@ fi
 
 # default parallel build jobs: number of CPUs available + 1
 if [ -z "${B2_JOBS:-}" ]; then
-    pythonexecutable=$(get_python_executable)
-    cpus=$(grep -c 'processor' /proc/cpuinfo || $pythonexecutable -c 'import multiprocessing as mp; print(mp.cpu_count())' || echo "2")
+    if ! cpus=$(grep -c 'processor' /proc/cpuinfo); then
+        if pythonexecutable=$(get_python_executable); then
+            cpus=$($pythonexecutable -c 'import multiprocessing as mp; print(mp.cpu_count())' || echo "2")
+        else
+            cpus=2
+        fi
+    fi
     export B2_JOBS=$((cpus + 1))
 fi
 
@@ -97,14 +102,9 @@ if ((${B2_CI_VERSION:-1} > 0)); then
 
   B2_ARGS=(
       ${B2_TOOLSET:+"toolset=$B2_TOOLSET"}
-      "cxxstd=$B2_CXXSTD"
+      ${B2_CXXSTD:+"cxxstd=$B2_CXXSTD"}
       ${B2_CXXFLAGS:+"cxxflags=$B2_CXXFLAGS"}
-  )
-  append_b2_args B2_DEFINES define
-  append_b2_args B2_INCLUDE include
-  # shellcheck disable=SC2206
-  B2_ARGS=(
-      "${B2_ARGS[@]}"
+      ${B2_TARGET_OS:+"target-os=$B2_TARGET_OS"}
       ${B2_LINKFLAGS:+"linkflags=$B2_LINKFLAGS"}
       ${B2_TESTFLAGS:-}
       ${B2_ADDRESS_MODEL:+address-model=$B2_ADDRESS_MODEL}
@@ -117,8 +117,16 @@ if ((${B2_CI_VERSION:-1} > 0)); then
       ${B2_TSAN:+thread-sanitizer=norecover}
       ${B2_UBSAN:+undefined-sanitizer=norecover}
       -j"${B2_JOBS}"
-      ${B2_FLAGS:-}
   )
+  append_b2_args B2_DEFINES define
+  append_b2_args B2_INCLUDE include
+  # clang-win requires to use the linker for the manifest
+  if [[ ${B2_TOOLSET:-} == "clang-win" ]]; then
+    B2_ARGS+=(embed-manifest-via=linker)
+  fi
+
+  # shellcheck disable=SC2206
+  B2_ARGS+=(${B2_FLAGS:-})
 else
   # Legacy codepath for compatibility for for old versions of the .github/*.yml files:
   # In (most) variables the prefix (such as "cxxflags=" for B2_CXXFLAGS) was included in the value, so it isn't added (again) here
